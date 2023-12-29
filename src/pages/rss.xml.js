7823166bb55c10afb0bf2499aff4dbb439ae1bf4
James Colesanti <colesanti.j@gmail.com>
2023-12-29 15:11:58 -0500

    Test

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,12 +3,12 @@ import { SITE_TITLE, SITE_DESCRIPTION } from "../config";
 import { getCollection } from "astro:content";
 
 export async function get(context) {
-  const blog = await getCollection("projects");
+  const projects = await getCollection("projects");
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: import.meta.env.SITE,
-    items: blog.map((post) => ({
+    items: projects.map((post) => ({
       title: post.data.title,
       pubDate: post.data.pubDate,
       description: post.data.description,
